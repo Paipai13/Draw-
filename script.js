e90f7f3da27feaefdcc8cb1038d646f7fdf310ab
Paige A Wright <43116855+Paipai13@users.noreply.github.com>
2022-08-03 08:59:59 +0000

    Added back mobile functionality but commented out, in case anyone can fix

--- script.js
+++ script.js
@@ -86,3 +86,44 @@ function updateSize() {
   sizeEl.innerText = size;
 }
 
+// mobile functionality
+
+//var heightRatio = 1.5;
+//canvas.height = canvas.width * heightRatio;
+//var mousePos = { x: 0, y: 0 };
+//ar lastPos = mousePos;
+
+//canvas.addEventListener('touchstart', function(e) {
+  //e.preventDefault();
+ // mousePos = getTouchPos(canvas, e);
+ // var touch = e.touches[0];
+ // var mouseEvent = new MouseEvent("mousedown", {
+ //   clientX: touch.clientX,
+ //   clientY: touch.clientY
+ // });
+ // canvas.dispatchEvent(mouseEvent);
+//}, false);
+
+//canvas.addEventListener('touchend', function(e) {
+ // e.preventDefault();
+ // var mouseEvent = new MouseEvent("mouseup", {});
+ // canvas.dispatchEvent(mouseEvent);
+//}, false);
+
+//canvas.addEventListener('touchmove', function(e) {
+ // e.preventDefault();
+ // var touch = e.touches[0];
+ // var mouseEvent = new MouseEvent("mousemove", {
+ //   clientX: touch.clientX,
+ //   clientY: touch.clientY
+ // });
+ // canvas.dispatchEvent(mouseEvent);
+//}, false);
+
+//function getTouchPos(canvasDom, touchEvent) {
+ // var rect = canvasDom.getBoundingClientRect();
+ // return {
+ //   x: touchEvent.touches[0].clientX - rect.left,
+ //   y: touchEvent.touches[0].clientY - rect.top
+ // };
+//}
